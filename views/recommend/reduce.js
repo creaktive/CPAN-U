4c34bca49ea1e81022ee6aa701fb2dc2cd05ca8a
Stanislaw Pusep <creaktive@gmail.com>
2012-12-11 01:22:42 -0200

    reduce fixed

--- views/recommend/reduce.js
+++ views/recommend/reduce.js
@@ -1,10 +1,43 @@
 function (keys, values, rereduce) {
     var rv = {};
-    for (i in values) {
+
+    for (var i = 0; i < values.length; i++) {
         var value = values[i];
-        for (k in value) {
-            rv[k] = value[k];
+        for (var k in value) {
+            if (value.hasOwnProperty(k)) {
+                if (rv.hasOwnProperty(k)) {
+                    rv[k] = Math.max(value[k], rv[k]);
+                } else {
+                    rv[k] = value[k];
+                }
+            }
         }
     }
-    return rv;
+
+    var rvk = [];
+    for (var j in rv) {
+        if (rv.hasOwnProperty(j)) {
+            rvk.push(j);
+        }
+    }
+
+    rvk = rvk.sort(function (b, a) {
+        var delta = rv[a] - rv[b];
+        if (delta) {
+            return delta;
+        } else if (a > b) {
+            return -1;
+        } else if (a < b) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }).splice(0, 10);
+
+    var clean = {};
+    for (var i = 0; i < rvk.length; i++) {
+        clean[rvk[i]] = rv[rvk[i]];
+    }
+
+    return clean;
 }
